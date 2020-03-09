6e79c05b4d7f68ddb03b7b703ff1221d9efa5097
herrerab <herrerab@usc.edu>
2020-03-09 14:00:26 -0700

    some improvements added

--- run_bach.py
+++ run_bach.py
@@ -23,28 +23,53 @@ print(command)
 os.system(command)
 cont = 0
 while (cont ==0):
- folder_to_read= input("Folder path to read(e.g. inside benchfolder type: ISCAS85/): ")
+ file_extension= input("files extension (e.g. bench): ")
+ folder_to_read= input("Folder path to read(e.g. nested inside benchfolder: e.g. ISCAS85/: ")
+ 
+ 
+ if (folder_to_read[-1] != "/"):
+  folder_to_read = folder_to_read+"/"
  if (os.path.isdir(working_directory+"/benchfolder/"+folder_to_read) == True):
-  cont =1
   folder_to_read = "benchfolder/"+folder_to_read
+  files=glob.glob(folder_to_read + "*." + file_extension,recursive=True)
+  #print(files)
+  file_list={x.replace(folder_to_read, '') for x in files}
+  #print(file_list)
+  total_files=len(file_list)
+  print("Inside "+folder_to_read+": "+ str(total_files)+" files with extension "+file_extension)
+  cont =int(input("that is correct? (1) try again(0): "))
+  #
  else: 
   print ("path not found, try again")
-file_extension= input("files extension (e.g. bench): ")
-command_p = input("command to execute on files (e.g. map_approx): ")
-parameters = input("command parameters (e.g. -r 0.05 -p): ")
+#
+cont = 0
+while (cont == 0):
+ command_p = input("command to execute on files (e.g. map_approx): ")
+ parameters = input("command parameters (e.g. -r 0.05 -p): ")
+ cont = int(input ("Command: " + command_p + " " + parameters + ": Correct? (1) Try again(0) "))
+#
 cont = 0
 while (cont == 0):
  folder_to_write= input("path to output folder (e.g. results/ISCAS85/): ")
+ if (folder_to_write[-1] != "/"):
+  folder_to_write=folder_to_write+"/"
 #
 #checking the destination folder
 #
 #print(os.path.isdir(working_directory+"/"+folder_to_write))
- if (os.path.isdir(working_directory+"/"+folder_to_write) == True):
-  print(working_directory+"/"+folder_to_write+" exists, saving results inside.")
-#  cont = 1
-  if (len(os.listdir(working_directory+"/"+folder_to_write)) !=0):
-   print(" Warning:destination directory is not empty. Some files can be overwrite.")
-   cont = int(input("proceed(1) or try another path(0)? "))
+ dest_dir = working_directory+"/"+folder_to_write
+ if (os.path.isdir(dest_dir) == True):
+
+  print(dest_dir+" exists, saving results inside.")
+  cont = 1
+  if (len(os.listdir(dest_dir)) !=0):
+   print(" Warning:destination directory is not empty.")
+   cont = int(input("Erase previous files: proceed(1) or try another path(0)? "))
+   if (cont == 1):
+   # erasing fies inside destination folder   
+    filestoremove = [os.path.join(dest_dir,fil) for fil in os.listdir(dest_dir)]
+    for fil in filestoremove:
+     os.remove(fil)   
  else:
   print (working_directory+"/"+folder_to_write+" does not exist, creating the folder...")
   os.makedirs(working_directory+"/"+folder_to_write)
@@ -59,9 +84,14 @@ while (cont == 0):
 file_index = 0
 #
 files=glob.glob(folder_to_read + "*." + file_extension,recursive=True)
+print(files)
 file_list={x.replace(folder_to_read, '') for x in files}
-#print(file_list)
+#
+print(file_list)
 total_files=len(file_list)
+#
+print("total files: "+str(total_files))
+#
 for file_name in file_list:
  file_index = file_index + 1
  print("Executing file "+ str(file_index) + " out of " + str(total_files))
